ca6160c581e24ddb281739ab0cfdc3ba391f902d
Lars Poppe <lars.poppe@tu-dortmund.de>
2021-12-16 16:07:58 +0100

    part 2 oo

--- 04/04.py
+++ 04/04.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 
 def parse_data(data):
     instr = list(np.array(data[0].split(',')).astype('int'))
@@ -19,9 +20,12 @@ def parse_data(data):
     boards = np.array(boards)
     return boards, instr
 
+
 def one(boards, instr):
     msk = np.zeros(boards.shape).astype(bool)
     nx = boards[0].shape[0]
+    
+    instr = deepcopy(instr)
     while len(instr) > 0:
         ins = instr.pop(0)
         idx = np.where(boards == ins)
@@ -45,14 +49,87 @@ def one(boards, instr):
     
     return board_win[~msk_win].sum() * ins
 
+
 def test_one():
     data = open('test_input.txt', 'r').read().splitlines()
     boards, instr = parse_data(data)
     
     assert one(boards, instr) == 4512
 
+
+def test_two():
+    data = open('test_input.txt', 'r').read().splitlines()
+    boards, instr = parse_data(data)
+    
+    assert two(boards, instr) == 1924    
+
+    
+class Board():
+    def __init__(self, config):
+        self.config = config
+        self.solved = False
+        self.count = 0
+        self.last_ins = None
+        self.msk = np.zeros(config.shape).astype(bool)
+        
+    def update_count(self):
+        self.count += 1
+        
+    def update(self, ins):
+        idx = np.where(self.config == ins)
+        self.msk[idx] = True
+        self.last_ins = ins
+        self.update_count()
+            
+    def check_win(self):
+        solved = False
+        sum_cols = (self.msk.sum(axis = 0) == self.config.shape[0])
+        sum_rows = (self.msk.sum(axis = 1) == self.config.shape[0])
+        
+        if sum_cols.any():
+            self.solved = True
+        elif sum_rows.any():
+            self.solved = True
+        else:
+            pass
+        return self.solved
+    
+    def get_score(self):
+        unmasked = self.config[~self.msk]
+        return unmasked.sum()
+    
+    def return_last(self):
+        return self.last_ins
+    
+    def return_count(self):
+        return self.count
+
+    
+def two(boards, instr):
+    bs = [Board(boards[i]) for i in range(len(boards))]
+    for b in bs:
+        for ins in instr:
+            b.update(ins)
+            solved = b.check_win()
+
+            if solved:
+                break
+    
+    # extract
+    lasts = np.array([b.return_last() for b in bs])
+    counts = np.array([b.return_count() for b in bs])
+    scores = np.array([b.get_score() for b in bs])
+    
+    # last index
+    last_idx = np.where(counts == np.max(counts))
+    
+    return (lasts[last_idx] * scores[last_idx])[0]
+
+
 if __name__ == '__main__':
     data = open('input.txt', 'r').read().splitlines()
     boards, instr = parse_data(data)
     test_one()
+    test_two()
     print(one(boards, instr))
+    print(two(boards, instr))
